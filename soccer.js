37450fc33739afa8664c6e2734b2d01d98c09127
Juan Ignacio Serra <jiserra@gmail.com>
2016-11-18 15:17:16 -0500

    First meetup code

---
+++ soccer.js
@@ -0,0 +1,19 @@
+var countries = [
+    {
+      "name": "Argentina",
+      "flag": "http://nationalflagstore.com/wp-content/uploads/2014/07/argentina-flag1.jpg",
+      "score": "5"
+    },
+    {
+      "name": "Colombia",
+      "flag": "http://www.all-flags-world.com/country-flag/Colombia/flag-colombia-L.jpg",
+      "score": "0"
+    }
+  ]
+
+  var something = false;
+
+  module.exports = {
+    countries: countries,
+    something: something
+  }
