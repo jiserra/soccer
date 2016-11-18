f3cd8a4e5c7e15b88bd1bb1392a7b916295c5e53
Juan Ignacio Serra <jiserra@gmail.com>
2016-11-18 15:28:23 -0500

    Oops, wrong code added before!

---
+++ main.js
@@ -0,0 +1,16 @@
+var countries = [
+  {
+    "name": "Argentina",
+    "score": "0",
+    "flag": "http://argentinaflag.facts.co/argentinaflagimage1.png"
+  },
+  {
+    "name": "Colombia",
+    "score": "0",
+    "flag": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Colombia.svg/255px-Flag_of_Colombia.svg.png"
+  }
+]
+
+module.exports = {
+  countries: countries
+};
